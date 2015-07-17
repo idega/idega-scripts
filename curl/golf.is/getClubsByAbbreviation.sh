6c4a5d8f48d0216deb1fb6cb84ac6fa71a8a66b0
lapiukshtiss <martynas.stake@gmail.com>
2015-07-17 11:34:12 +0300

    Test for getting club by abbreviation

---
+++ curl/golf.is/getClubsByAbbreviation.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+printf '#############################################################\n'
+printf '# A script to test Clubs#getXMLOrJSON on                    #\n'
+printf '# http://www.golf.is/rest/clubs                                 #\n'
+printf '# By Martynas Stakė (martynas@idega.is) 2015                #\n'
+printf '#############################################################\n'
+printf '\n'
+
+curl --insecure \
+    --include \
+    --request GET \
+    http://www.golf.is/rest/clubs/gkg
+

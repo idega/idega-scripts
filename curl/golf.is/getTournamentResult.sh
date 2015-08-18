91633824640b9c1f539e9372a84ad5daf9733e50
lapiukshtiss <martynas.stake@gmail.com>
2015-08-18 12:49:05 +0300

    Changing the links for tournament results

--- curl/golf.is/getTournamentResult.sh
+++ curl/golf.is/getTournamentResult.sh
@@ -2,7 +2,7 @@
 
 printf '#############################################################\n'
 printf '# A script to test TournamentResult#getXMLOrJSON on         #\n'
-printf '# http://www.golf.is/rest/tournamentResult                  #\n'  
+printf '# http://www.golf.is/api/tournamentResult                  #\n'  
 printf '# By Martynas Stakė (martynas@idega.is) 2015                #\n'
 printf '#############################################################\n'
 printf '\n'
@@ -11,4 +11,4 @@ curl --insecure \
     --include \
     --request GET \
     --header "Accept: application/json" \
-    http://www.golf.is/rest/tournamentResult/?tournamentID=20862
+    http://www.golf.is/api/tournamentResult/?tournamentID=20862
--- curl/golf.is/getTournamentResults.sh
+++ curl/golf.is/getTournamentResults.sh
@@ -2,7 +2,7 @@
 
 printf '#############################################################\n'
 printf '# A script to test TournamentServices#getResults on         #\n'
-printf '# http://www.golf.is/rest/tournament/results                    #\n'
+printf '# http://www.golf.is/api/tournament/results                 #\n'
 printf '# By Martynas Stakė (martynas@idega.is) 2015                #\n'
 printf '#############################################################\n'
 printf '\n'
@@ -11,4 +11,4 @@ curl --insecure \
     --include \
     --request GET \
     --header "Accept: application/json" \
-    "http://www.golf.is/rest/tournament/results?tourId=20862"
+    "http://www.golf.is/api/tournament/results?tourId=20862"

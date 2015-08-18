26630bd57be90de4f1e6f1d97bfe67d4c1acb2f1
lapiukshtiss <martynas.stake@gmail.com>
2015-08-18 12:39:30 +0300

    Fixed for filed and clubs testing

--- curl/golf.is/getFields.sh
+++ curl/golf.is/getFields.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 printf '#############################################################\n'
-printf '# A script to test Clubs#getXMLOrJSON on                    #\n'
-printf '# http://www.golf.is/rest/clubs                                 #\n'  
+printf '# A script to test FieldWebservice#getFields on             #\n'
+printf '# http://www.golf.is/api/fields                             #\n'
 printf '# By Martynas Stakė (martynas@idega.is) 2015                #\n'
 printf '#############################################################\n'
 printf '\n'
@@ -10,4 +10,4 @@ printf '\n'
 curl --insecure \
     --include \
     --request GET \
-    "http://www.golf.is/rest/clubs?personalID=2903612499&abbreviation=gkg"
+    "http://www.golf.is/api/fields?personalID=2903612499&holes=18&name=dalu&unionID=84"

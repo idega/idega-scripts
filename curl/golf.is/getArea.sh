4367d209f3f4044b592578c91406b289288848bc
lapiukshtiss <martynas.stake@gmail.com>
2015-08-19 12:12:03 +0300

    Test and result for area search

---
+++ curl/golf.is/getArea.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+printf '#############################################################\n'
+printf '# A script to test Areas#getAreas on                        #\n'
+printf '# http://www.golf.is/rest/areas                             #\n'  
+printf '# By Martynas Stakė (martynas@idega.is) 2015                #\n'
+printf '#############################################################\n'
+printf '\n'
+
+curl --insecure \
+    --include \
+    --request GET \
+    --header "Accept: application/json" \
+    http://www.golf.is/rest/areas/9 >> area.json

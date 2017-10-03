2ffb5f534a054e9568aecc1e3e1db9febc132ac7
L4p1n45 <martynas.stake@gmail.com>
2017-10-03 10:15:56 +0300

    Small fixes

--- CentOS/home/idegaweb/scripts/fail2ban.sh
+++ CentOS/home/idegaweb/scripts/fail2ban.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+echo "#########################################################################"
+echo "# A script to set up fail2ban                                           #"
+echo "# By Martynas Stakė (martynas@idega.is) 2017                            #"
+echo "#########################################################################"
+echo "\n"
 sudo yum remove -y postfix
 
 sudo yum install -y fail2ban
-sudo cd /etc/fail2ban/
+cd /etc/fail2ban/
 sudo wget https://raw.githubusercontent.com/idega/idega-scripts/master/CentOS/etc/fail2ban/jail.local
 
-sudo cd /etc/fail2ban/jail.d/
+cd /etc/fail2ban/jail.d/
 sudo wget https://raw.githubusercontent.com/idega/idega-scripts/master/CentOS/etc/fail2ban/jail.d/apache.conf
 sudo wget https://raw.githubusercontent.com/idega/idega-scripts/master/CentOS/etc/fail2ban/jail.d/firewalld.conf
 sudo wget https://raw.githubusercontent.com/idega/idega-scripts/master/CentOS/etc/fail2ban/jail.d/mysql.conf
@@ -20,6 +25,9 @@ sudo systemctl restart fail2ban
 sudo yum install -y sendmail
 sudo yum install -y sendmail-cf
 sudo yum install -y cyrus-sasl-plain
+
+# Now we need to apply this before continue
+# https://sendgrid.com/docs/Integrate/Mail_Servers/sendmail.html
 
 cd /etc/mail
 m4 sendmail.mc > sendmail.cf

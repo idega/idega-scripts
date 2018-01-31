3b04af320de13c28bf572e164c8eb371e4199c34
L4p1n45 <martynas.stake@gmail.com>
2018-01-31 17:28:28 +0200

    Some minor changes

--- CentOS/home/idegaweb/tomcat/logs/find_string_in_access_log.sh
+++ CentOS/home/idegaweb/tomcat/logs/find_string_in_access_log.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 OUTPUT_FILE_NAME='result.out';
 
-PATH_SELECTION=( 'api' 'rest' )
+PATH_SELECTION=( api rest )
 
-SOURCE_FILE_EXPRESSION='localhost_access_log.*.txt';
-SOURCE_FILE_NAMES=(`ls $SOURCE_FILE_EXPRESSION`);
+SOURCE_FILE_NAMES=(`find $PWD -name 'localhost_access_log.*'`);
 
 for SOURCE_FILE_NAME in "${SOURCE_FILE_NAMES[@]}"
 do
         echo "Processing: $SOURCE_FILE_NAME";
         for PATH_VALUE in "${PATH_SELECTION[@]}"
         do
-                sed -n '/$PATH_VALUE/p' $SOURCE_FILE_NAME >> $OUTPUT_FILE_NAME;
+                echo "  Path: $PATH_VALUE";
+                sed -n '/'$PATH_VALUE'/p' $SOURCE_FILE_NAME >> $OUTPUT_FILE_NAME;
         done
 done

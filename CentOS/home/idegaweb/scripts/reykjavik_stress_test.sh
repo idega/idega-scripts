ec9250c087e1cab954100f11618220e1cb267547
L4p1n45 <martynas.stake@gmail.com>
2018-02-20 15:08:25 +0200

    Output fix

--- CentOS/home/idegaweb/scripts/reykjavik_stress_test.sh
+++ CentOS/home/idegaweb/scripts/reykjavik_stress_test.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-REQUESTS_AMOUNT=100;
-USERS_AMOUNT=500;
+REQUESTS_AMOUNT=2;
+USERS_AMOUNT=2;
 COUNTER=1;
 
 echo "$REQUESTS_AMOUNT requests are used:";
 while [  $COUNTER -lt $USERS_AMOUNT ];
 do
 	echo "User $COUNTER in action:";
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=login&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/hlutverk?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/notandi/sidanmin/?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=29&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=1741&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=logoff&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	time curl -O -s "https://rafraen.reykjavik.is/pages?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	time curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=login&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	time curl -O -s "https://rafraen.reykjavik.is/pages/hlutverk?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	time curl -O -s "https://rafraen.reykjavik.is/pages/notandi/sidanmin/?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=29&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=1741&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	time curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=logoff&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
 	let COUNTER=COUNTER+1;
 done;
 

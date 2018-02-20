182a6c2b00b88c80230273318bcd69f14ad8c6c5
L4p1n45 <martynas.stake@gmail.com>
2018-02-20 15:07:17 +0200

    Fix for MAC

--- CentOS/home/idegaweb/scripts/reykjavik_stress_test.sh
+++ CentOS/home/idegaweb/scripts/reykjavik_stress_test.sh
@@ -7,14 +7,19 @@ echo "$REQUESTS_AMOUNT requests are used:";
 while [  $COUNTER -lt $USERS_AMOUNT ];
 do
 	echo "User $COUNTER in action:";
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages?[1-$REQUESTS_AMOUNT]"
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=login&[1-$REQUESTS_AMOUNT]"
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/hlutverk?[1-$REQUESTS_AMOUNT]"
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/notandi/sidanmin/?[1-$REQUESTS_AMOUNT]"
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-$REQUESTS_AMOUNT]"
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=29&[1-$REQUESTS_AMOUNT]"
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=1741&[1-$REQUESTS_AMOUNT]"
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-$REQUESTS_AMOUNT]"
-	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=logoff&[1-$REQUESTS_AMOUNT]"
+	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=login&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/hlutverk?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/notandi/sidanmin/?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=29&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=1741&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
+	/usr/bin/time curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=logoff&[1-$REQUESTS_AMOUNT]" & pidlist="$pidlist $!" 
 	let COUNTER=COUNTER+1;
 done;
+
+for job in $pidlist do 
+  echo $job
+  wait $job || let "FAIL+=1" 
+done

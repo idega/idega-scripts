3257dd206986dec9f71cfbf050311f8f4cd34ee1
L4p1n45 <martynas.stake@gmail.com>
2018-02-20 14:59:02 +0200

    Updated output for stress test

--- CentOS/home/idegaweb/scripts/reykjavik_stress_test.sh
+++ CentOS/home/idegaweb/scripts/reykjavik_stress_test.sh
@@ -1,42 +1,20 @@
 #!/bin/bash
+REQUESTS_AMOUNT=2;
+USERS_AMOUNT=5;
+COUNTER=1;
 
-for i in `seq 1 10`;
+echo "$REQUESTS_AMOUNT requests are used:";
+while [  $COUNTER -lt $USERS_AMOUNT ];
 do
-	time curl -O -s "https://rafraen.reykjavik.is/pages?[1-1000]" &
-	pidlist="$pidlist $!"
-
-	time curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=login&[1-1000]" &
-	pidlist="$pidlist $!"
-
-	time curl -O -s "https://rafraen.reykjavik.is/pages/hlutverk?[1-1000]" &
-	pidlist="$pidlist $!"
-
-	time curl -O -s "https://rafraen.reykjavik.is/pages/notandi/sidanmin/?[1-1000]" &
-	pidlist="$pidlist $!"
-
-	time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-1000]" &
-	pidlist="$pidlist $!"
-
-	time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=29&[1-1000]" &
-	pidlist="$pidlist $!"
-
-	time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=1741&[1-1000]" &
-	pidlist="$pidlist $!"
-
-	time curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-1000]" &
-	pidlist="$pidlist $!"
-
-	time curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=logoff&[1-1000]" &
-	pidlist="$pidlist $!"
-done
-
-for job in $pidlist do 
-  echo $job
-  wait $job || let "FAIL+=1"
-done
-
-if [ "$FAIL" == "0" ]; then
-  echo "YAY!"
-else 
-  echo "FAIL! ($FAIL)"
-fi
+	echo "User $COUNTER in action:";
+	/usr/bin/time -f "Execution time for https://rafraen.reykjavik.is/pages %e seconds"  curl -O -s "https://rafraen.reykjavik.is/pages?[1-$REQUESTS_AMOUNT]"
+	/usr/bin/time -f "Execution time for https://rafraen.reykjavik.is/pages/login %e seconds"  curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=login&[1-$REQUESTS_AMOUNT]"
+	/usr/bin/time -f "Execution time for https://rafraen.reykjavik.is/pages/hlutverk %e seconds"  curl -O -s "https://rafraen.reykjavik.is/pages/hlutverk?[1-$REQUESTS_AMOUNT]"
+	/usr/bin/time -f "Execution time for https://rafraen.reykjavik.is/pages/notandi/sidanmin/ %e seconds"  curl -O -s "https://rafraen.reykjavik.is/pages/notandi/sidanmin/?[1-$REQUESTS_AMOUNT]"
+	/usr/bin/time -f "Execution time for https://rafraen.reykjavik.is/pages/umsoknir/ %e seconds"  curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-$REQUESTS_AMOUNT]"
+	/usr/bin/time -f "Execution time for https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=29 %e seconds"  curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=29&[1-$REQUESTS_AMOUNT]"
+	/usr/bin/time -f "Execution time for https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=1741 %e seconds"  curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?prm_app_application_pk=1741&[1-$REQUESTS_AMOUNT]"
+	/usr/bin/time -f "Execution time for https://rafraen.reykjavik.is/pages/umsoknir/ %e seconds"  curl -O -s "https://rafraen.reykjavik.is/pages/umsoknir/?[1-$REQUESTS_AMOUNT]"
+	/usr/bin/time -f "Execution time for https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=logoff %e seconds"  curl -O -s "https://rafraen.reykjavik.is/pages/?l_by_uuid=a8e95706-3d10-4038-8359-adabdb8bb470&login_state=logoff&[1-$REQUESTS_AMOUNT]"
+	let COUNTER=COUNTER+1;
+done;

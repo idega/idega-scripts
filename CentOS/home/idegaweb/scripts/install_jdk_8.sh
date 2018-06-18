ca96c85423f277042391279daf308f1cdc478cb5
Valdas Žemaitis <valdas@idega.com>
2018-06-18 10:31:59 +0300

    Updating JDK version

--- CentOS/home/idegaweb/scripts/install_jdk_8.sh
+++ CentOS/home/idegaweb/scripts/install_jdk_8.sh
@@ -7,6 +7,6 @@ echo "#########################################################################"
 echo "\n"
 
 echo "# Installing Java Developement Kit 8..."
-s3cmd get s3://idega-backup/rpm/jdk-8u60-linux-x64.rpm
-sudo rpm -i jdk-8u60-linux-x64.rpm
-rm -rf jdk-8u60-linux-x64.rpm
+s3cmd get s3://idega-backup/rpm/jdk-8u171-linux-x64.rpm
+sudo rpm -i jdk-8u171-linux-x64.rpm
+rm -rf jdk-8u171-linux-x64.rpm

5ff8eb403315ad58e7b43c8f3520f0cc3d57e695
Martynas Stake <idegaweb@dev.lan>
2020-09-01 13:16:07 +0300

    Script improvement

--- CentOS/root/scripts/scap/scan.sh
+++ CentOS/root/scripts/scap/scan.sh
@@ -6,5 +6,4 @@
         --tailoring-file /root/scripts/scap/content/ssg-centos8-ds-tailoring.xml  \
         --results scan-results.xml /root/scripts/scap/content/ssg-centos8-ds.xml
 /usr/bin/oscap xccdf generate report /root/scripts/scap/scan-results.xml > /root/scripts/scap/scan-report.xhtml
-/bin/mailx -r "root@$(hostname)" -s "$(echo -e "$(hostname) - SCAP report (custom) \nContent-Type: text/html")" martynas@dav.lt < /root/scripts/scap/scan-report.xhtml
-
+/bin/mailx -s "SCAP report (custom)" martynas@dav.lt < /root/scripts/scap/scan-report.xhtml
